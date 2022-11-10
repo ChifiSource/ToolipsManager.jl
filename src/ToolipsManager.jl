b893fe59c1f15d7a95d8953f07bbaaee4db51ae2
emmy <52672675+emmettgb@users.noreply.github.com>
2022-11-10 05:37:55 -0600

    the form I am going with

--- src/ToolipsManager.jl
+++ src/ToolipsManager.jl
@@ -1,15 +1,46 @@
 module ToolipsManager
 using Toolips
+import Toolips: ServerExtension, ToolipsServer
 using ToolipsRemote
+using ToolipsAuth
 
-function ManagementServer(ip::String, port::Int64, hostname::String;
-routes::Vector{<: Toolips.AbstractRoute} = Vector{Toolips.AbstractRoute}(),
-extensions::Vector{<: Toolips.ServerExtension} = Vector{Toolips.ServerExtension}()
-    )
-    push!(extensions, ManagerRemote())
-    WebServer(ip, port, hostname = hostname, routes = routes,
-    extensions = extensions)
+add_management(ws::WebServer, key::String;
+    motd = """##### please login:""") = begin
+    push!(ws, manager_remote(ws.hostname, key, motd))
 end
 
+function manager_remote(name::String, key::String, motd::String)
+    remotefunctions = Dict(5 => manager_controller())
+    users = [name => key => 5]
+    Remote(remotefunction, users; motd = motd)
+end
 
+function manager_controller(controls::Dict{String, Function} = Dict(
+    "?" => manager_help, "overview" => overview,
+    "manage internals" => manage_internals))
+end
+
+function manager_help(args::Vector{String}, c::RemoteConnection)
+
+end
+
+function overview()
+
+end
+
+function manage_internals()
+
+end
+
+mutable struct ManagementServer <: ToolipsServer
+    ip::String
+    hostname::String
+    port::Int64
+    routes::Vector{AbstractRoute}
+    extensions::Vector{ServerExtension}
+    servers::Vector{ToolipsServer}
+    function ManagementServer()
+
+    end
+end
 end # module
