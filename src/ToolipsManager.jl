fa27687e73fc9f6e9eafc0b3af6975bc4de0ff63
emmy <52672675+emmettgb@users.noreply.github.com>
2023-02-07 04:29:08 -0600

    project outline

--- src/ToolipsManager.jl
+++ src/ToolipsManager.jl
@@ -2,35 +2,57 @@ module ToolipsManager
 using Toolips
 import Toolips: ServerExtension, ToolipsServer
 using ToolipsRemote
-using ToolipsAuth
+using ToolipsSession
 
-add_management(ws::WebServer, key::String;
-    motd = """##### please login:""") = begin
+add_management!(ws::ToolipsServer, key::String;
+    motd::String = """##### please login:""") = begin
     push!(ws, manager_remote(ws.hostname, key, motd))
 end
 
+universal_controls = Dict{String, Function}(
+    "?" => manager_help, "status" => status,
+    "manage" => management, "check" => check)
+
 function manager_remote(name::String, key::String, motd::String)
-    remotefunctions = Dict(5 => manager_controller())
+    remotefunctions = Dict(5 => ToolipsRemote.controller(universal_controls))
     users = [name => key => 5]
     Remote(remotefunction, users; motd = motd)
 end
 
-function manager_controller(controls::Dict{String, Function} = Dict(
-    "?" => manager_help, "overview" => overview,
-    "manage internals" => manage_internals))
+function management_server_remote(name::String, key::String, motd::String)
+    contr = copy(universal_controls)
+    push!(contr, "overview" => overview)
+    ToolipsRemote.controller()
+    remotefunctions = Dict(5 => )
+    users = [name => key => 5]
+    Remote(remotefunction, users; motd = motd)
 end
 
 function manager_help(args::Vector{String}, c::RemoteConnection)
 
 end
 
-function overview()
+function overview(args::Vector{String}, c::RemoteConnection)
 
 end
 
-function manage_internals()
+function status(args::Vector{String}, c::remoteConnection)
 
 end
+
+function check(args::Vector{String}, c::RemoteConnection)
+
+end
+
+function management(args::Vector{String}, c::RemoteConnection)
+
+end
+
+function manage(c::Connection)
+
+end
+
+
 
 mutable struct ManagementServer <: ToolipsServer
     ip::String
@@ -38,9 +60,20 @@ mutable struct ManagementServer <: ToolipsServer
     port::Int64
     routes::Vector{AbstractRoute}
     extensions::Vector{ServerExtension}
-    servers::Vector{ToolipsServer}
-    function ManagementServer()
-
+    servers::Vector{Toolips.WebServer}
+    start::Function
+    function ManagementServer(servers::Vector{Toolips.WebServer},
+        usrpwd::Pair{String, String},
+        host::String = "127.0.0.1", port::Int64 = 8000;
+        hostname::String = "", routes::Vector{AbstractRoute} = routes(route("/",
+        (c::Connection) -> write!(c, p(text = "Hello world!")))),
+        extensions::Vector{ServerExtension})
+        push!(extensions, management_server_remote(hostname))
+        start() = Toolips._start(host, port, routes, extensions, server, hostname)
+        new(host, hostname, port, routes, extensions, servers, start)::ManagementServer
     end
 end
+
+
+
 end # module
